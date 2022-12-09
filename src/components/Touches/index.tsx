806047765528d6bd894c6b26362ed62a5cbe7e09
¡Duque <atanaelleonardo@gmail.com>
2022-12-09 16:21:22 -0300

    𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝑻𝒐𝒖𝒄𝒉𝒆𝒔 𝑪𝒐𝒎𝒑𝒐𝒏𝒆𝒏𝒕

---
+++ src/components/Touches/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { styles } from "./styles";
+
+export const Touches = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box} />
+    </View>
+  );
+};

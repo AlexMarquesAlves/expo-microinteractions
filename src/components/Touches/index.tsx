b233889a019dfb802b88fd8af274edd21fe57fb2
¡Duque <atanaelleonardo@gmail.com>
2022-12-09 16:41:07 -0300

    𝑻𝒐𝒖𝒄𝒉𝒆𝒔 𝒘𝒊𝒕𝒉 𝑷𝒓𝒆𝒔𝒔𝒂𝒃𝒍𝒆

--- src/components/Touches/index.tsx
+++ src/components/Touches/index.tsx
@@ -1,10 +1,32 @@
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 import { styles } from "./styles";
 
 export const Touches = () => {
+  const position = useSharedValue(100);
+
+  function onPressIn(params: any) {
+    position.value = withSpring(150);
+  }
+
+  function onPressOut(params: any) {
+    position.value = withSpring(100);
+  }
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    width: position.value,
+    height: position.value,
+  }));
+
   return (
     <View style={styles.container}>
-      <View style={styles.box} />
+      <Pressable onPressIn={onPressIn} onPressOut={onPressOut}>
+        <Animated.View style={[styles.box, animatedStyle]} />
+      </Pressable>
     </View>
   );
 };
--- src/components/Touches/styles.ts
+++ src/components/Touches/styles.ts
@@ -7,8 +7,8 @@ export const styles = StyleSheet.create({
     alignItems: "center",
   },
   box: {
-    width: 100,
-    height: 100,
+    // width: 100, definido na animação
+    // height: 100, definido na animação
     borderRadius: 12,
     backgroundColor: "#8527e5",
   },

186cd93a632fa333be9214a8f6566e4046518bd9
¡Duque <atanaelleonardo@gmail.com>
2022-12-09 17:26:27 -0300

    𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝑷𝒂𝒏 𝒄𝒐𝒎𝒑𝒐𝒏𝒆𝒏𝒕

---
+++ src/components/Pan/index.tsx
@@ -0,0 +1,26 @@
+import { View } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
+import { styles } from "./styles";
+
+export const Pan = () => {
+  const scale = useSharedValue(1);
+  const rotationGesture = Gesture.Pinch().onUpdate((event) => {
+    scale.value = event.scale;
+  });
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+  }));
+
+  return (
+    <View style={styles.container}>
+      <GestureDetector gesture={rotationGesture}>
+        <Animated.View style={[styles.box, animatedStyle]} />
+      </GestureDetector>
+    </View>
+  );
+};
